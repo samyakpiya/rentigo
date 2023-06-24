12ea9185132eabbd7bc7898c7de8cdaf68ea2085
Samyak Piya <samyakpiya@gmail.com>
2023-06-24 16:37:24 -0400

    Complete Hero Section

---
+++ components/index.ts
@@ -0,0 +1,6 @@
+// Purpose: Export all components from this file to make importing simpler
+
+import Hero from "./Hero";
+import CustomButton from "./CustomButton";
+
+export { Hero, CustomButton };
