e9bba2bb327493b978f227b9ddda6170cc63092f
Samyak Piya <samyakpiya@gmail.com>
2023-06-24 18:01:41 -0400

    Complete Layout - Navbar & Footer

---
+++ constants/index.ts
@@ -0,0 +1,53 @@
+export const NavLinks = [
+  { url: "/", key: "Inspiration", text: "Inspiration" },
+  { url: "/", key: "Find Projects", text: "Find Projects" },
+  { url: "/", key: "Learn Development", text: "Learn Development" },
+  { url: "/", key: "Career Advancement", text: "Career Advancement" },
+  { url: "/", key: "Hire Developers", text: "Hire Developers" },
+];
+
+export const categoryFilters = [
+  "Frontend",
+  "Backend",
+  "Full-Stack",
+  "Mobile",
+  "UI/UX",
+  "Game Dev",
+  "DevOps",
+  "Data Science",
+  "Machine Learning",
+  "Cybersecurity",
+  "Blockchain",
+  "E-commerce",
+  "Chatbots",
+];
+
+export const footerLinks = [
+  {
+    title: "About",
+    links: [
+      { title: "How it works", url: "/" },
+      { title: "Featured", url: "/" },
+      { title: "Partnership", url: "/" },
+      { title: "Business Relation", url: "/" },
+    ],
+  },
+  {
+    title: "Company",
+    links: [
+      { title: "Events", url: "/" },
+      { title: "Blog", url: "/" },
+      { title: "Podcast", url: "/" },
+      { title: "Invite a friend", url: "/" },
+    ],
+  },
+  {
+    title: "Socials",
+    links: [
+      { title: "Discord", url: "/" },
+      { title: "Instagram", url: "/" },
+      { title: "Twitter", url: "/" },
+      { title: "Facebook", url: "/" },
+    ],
+  },
+];
